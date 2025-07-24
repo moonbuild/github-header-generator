e78e6a9414b244e7183b2fd509ca0aa3bf860e05
Mourya Pranay <styooodnt@gmail.com>
2025-07-24 12:38:41 +0530

    task #8 add copy/download image option

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,40 +1,140 @@
+import { useRef, useState } from 'react';
+import html2canvas from 'html2canvas';
+
 import { useEditorStore } from '../../stores/editorStore';
 
 import './header.css';
 
 const Header = () => {
+  const headerRef = useRef<HTMLDivElement>(null);
+
   const { mainText, highlightedText, backgroundColor, textColor } = useEditorStore();
+  const [copy, setCopy] = useState(true);
+
   const SecondaryLine = () => (
     <div className="background-text-wrapper">
-      {mainText.split(' ').map((text) => (
-        <span
-          className="background-text"
-          style={{ color: 'transparent', WebkitTextStroke: `1px ${textColor}` }}
-        >
-          {text}
-        </span>
-      ))}
+      {mainText
+        .replace('/d', '•')
+        .split(' ')
+        .map((text) => (
+          <span
+            key={text}
+            className="background-text"
+            style={{ color: 'transparent', WebkitTextStroke: `1px ${textColor}` }}
+          >
+            {text}
+          </span>
+        ))}
     </div>
   );
+  const downloadImage = async () => {
+    if (!headerRef.current) return;
+    const canvas = await html2canvas(headerRef.current);
+    const link = document.createElement('a');
+    link.download = 'github-header.png';
+    link.href = canvas.toDataURL('image/png');
+    link.click();
+  };
+
+  const copyImage = async () => {
+    if (!headerRef.current) return;
+    const canvas = await html2canvas(headerRef.current);
+    canvas.toBlob(async (blob) => {
+      if (!blob) return;
+      try {
+        const item = new ClipboardItem({ 'image/png': blob });
+        await navigator.clipboard.write([item]);
+        setCopy(false);
+        setTimeout(() => {
+          setCopy(true);
+        }, 2000);
+      } catch (err) {
+        console.log(err);
+      }
+    });
+  };
+
   return (
-    <div className="header">
-      <div className="banner" style={{ backgroundColor: backgroundColor }}>
-        <SecondaryLine />
-        <div className="main-text-wrapper">
-          {mainText.split(' ').map((text) => (
-            <span
-              className="main-text"
-              style={{
-                backgroundColor: `${text === highlightedText ? textColor : backgroundColor}`,
-                color: `${text === highlightedText ? backgroundColor : textColor}`,
-                padding: `${text === highlightedText ? '0px 2px' : ''}`,
-              }}
-            >
-              {text}
-            </span>
-          ))}
+    <div className="header-btns-wrapper">
+      <div className="header">
+        <div ref={headerRef} className="banner" style={{ backgroundColor: backgroundColor }}>
+          <SecondaryLine />
+          <div className="main-text-wrapper">
+            {mainText
+              .replace('/d', '•')
+              .split(' ')
+              .map((text) => (
+                <span
+                  className="main-text"
+                  style={{
+                    backgroundColor: `${text === highlightedText.replace('/d', '•') ? textColor : backgroundColor}`,
+                    color: `${text === highlightedText.replace('/d', '•') ? backgroundColor : textColor}`,
+                    padding: `${text === highlightedText.replace('/d', '•') ? '0px 2px' : ''}`,
+                  }}
+                >
+                  {text}
+                </span>
+              ))}
+          </div>
+          <SecondaryLine />
         </div>
-        <SecondaryLine />
+      </div>
+      <div className="buttons-container">
+        <button onClick={copyImage} className="copy-btn">
+          <>
+            {copy ? (
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="18"
+                height="18"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="currentColor"
+                stroke-width="2"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+              >
+                <rect width="14" height="14" x="8" y="8" rx="2" ry="2" />
+                <path d="M4 16c-1.1 0-2-.9-2-2V4c0-1.1.9-2 2-2h10c1.1 0 2 .9 2 2" />
+              </svg>
+            ) : (
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="18"
+                height="18"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="green"
+                stroke-width="2"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+              >
+                <path d="m12 15 2 2 4-4" />
+                <rect width="14" height="14" x="8" y="8" rx="2" ry="2" />
+                <path d="M4 16c-1.1 0-2-.9-2-2V4c0-1.1.9-2 2-2h10c1.1 0 2 .9 2 2" />
+              </svg>
+            )}
+          </>
+          Copy
+        </button>
+        <button onClick={downloadImage} className="download-btn">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="18"
+            height="18"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            stroke-width="2"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+          >
+            <path d="M15.2 3a2 2 0 0 1 1.4.6l3.8 3.8a2 2 0 0 1 .6 1.4V19a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2z" />
+            <path d="M17 21v-7a1 1 0 0 0-1-1H8a1 1 0 0 0-1 1v7" />
+            <path d="M7 3v4a1 1 0 0 0 1 1h7" />
+          </svg>
+          Download
+        </button>
       </div>
     </div>
   );
